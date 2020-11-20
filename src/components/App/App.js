2a79d01dfc995946eeac8b63a8fdb23daf529fd6
Alex Parij <alexander.parij@autodesk.com>
2020-11-20 16:18:43 -0500

    mod 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-
+import {createTiles} from '../../misc/utils';
 import './App.css';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      numTiles: 36,
+      playing: false,
+      previousTileIndex: null,
+      tiles: [],
+      toBeCleared: null,
+    };
+  }
 
-  render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Turbo-Matcher
-      </header>
-        <OptionsPanel />
-        <Board />
+  startGame(numTiles){
+    this.setState((state)=>{ 
+      return {
+        playing: true,
+        previousTileIndex: null,
+        toBeCleared: null,
+        tiles: createTiles(state.numTiles),
       }
-    </div>
-  );
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Turbo-Matcher
+        </header>
+          <OptionsPanel startGame={this.startGame} playing={this.state.playing} numTiles={this.state.numTiles}/>
+          <Board numTiles={this.state.numTiles} tiles={this.state.tiles}/>
+      </div>
+    );
 
   }
 }
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Board.css';
-
+import Tile from '../Tile';
 const Board = (props) => {
 
   const gridConfig = {
@@ -8,9 +8,11 @@ const Board = (props) => {
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
   }
 
+  const tiles = props.tiles.map(tile=> <Tile {...tile} />);
 
   return (
-    <div className='Board' style={gridConfig}>
+    <div className='Board' style={gridConfig} >
+      {tiles}
     </div>
   )
 }

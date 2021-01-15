bc8d37409976b4d9ec4ebb9ccfdc5933b632f875
Alex Parij <alexander.parij@autodesk.com>
2021-01-15 13:55:38 -0500

    module 3

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -7,7 +7,7 @@ const Tile = (props) => {
   const color = props.selected || props.matched ? {backgroundColor: props.color}: null;
 
   return (
-    <div style={color} className='Tile'>
+    <div style={color} className='Tile' onClick={()=>{props.handleTileClicked(props.id, props.color)}} >
       {props.selected || props.matched ? <props.svg /> : null }
     </div>
   )
